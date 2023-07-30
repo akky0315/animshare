0b8b705904f874f42fb2739672b19eb81d45bde5
akky0315 <akky.yoppy@gmail.com>
2023-07-31 01:44:42 +0900

    バグ修正8

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,8 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
+                'css/app.css',
+                'css/app.home.css',
                 'resources/js/app.js',
             ],
             refresh: true,
